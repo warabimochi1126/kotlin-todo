7e4d9aa9585cf4404cf835744c46435511b1cba1
warabimochi <satomi920003@yahoo.co.jp>
2023-07-07 01:30:30 +0900

    完成

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -34,6 +35,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -62,8 +64,6 @@ class MainActivity : ComponentActivity() {
 /* JetpackComposeを使ってUIを作成する場合はComposableという単位でUIを作成します(Composable=UIという意味でOK)
 *  Composableを作成する時は@Composableというアノテーションを付けて関数名の頭文字を大文字にします */
 
-// 追加したTextFieldという部品が試験的な物で今後変更される可能性があるという事を示すアノテーションです(付けてないとビルド出来ないです)
-@OptIn(ExperimentalMaterial3Api::class)
 // @Composableというアノテーションを追加
 @Composable
 /* ①UI用の関数を作成する */
@@ -72,7 +72,7 @@ fun MyTodoApp() {
     val todo = remember{ mutableStateOf("")}
 
     //todoの一覧は複数のテキストを扱うので状態を配列で持ちます.mutableStateListOf()は配列で状態を作成します.<String>で配列の型は文字列を指定しています
-    val todoList = remember { mutableStateListOf<String>() }
+    val todoList = remember { mutableStateListOf<Todo>() }
 
     MyTodoAppContent(todo = todo, todoList = todoList)
 }
@@ -80,8 +80,7 @@ fun MyTodoApp() {
 @Composable
 fun MyTodoAppContent(
     todo: MutableState<String>,
-
-    todoList: SnapshotStateList<String>
+    todoList: SnapshotStateList<Todo>
 ) {
     // タイトルをトップバーの中にいれて表示します.Scaffoldは画面全体のUIを組むために使います
     Scaffold(
@@ -124,18 +123,19 @@ fun MyTodoAppContent(
 
                 //todoList.add()で追加ボタンを押した時にフォームに入力されていたデータがtodoListに保存されます
                 Button(onClick = {
-                    todoList.add(todo.value)
+                    todoList.add(Todo(text = todo.value, isCompoleted = false))
                     todo.value = ""
                 }) {
                     Text(text = "追加")
                 }
             }
 
-            //追加したtodoの内容を表示しています
+            //追加したtodoの内容を表示しています.forEachIndexed()は引数indexに要素の順番.itemに要素自体を渡します
             todoList.forEachIndexed { index, item ->
                 TodoItem(
-                    text = item,
-                    deleteTodo = { todoList.removeAt(index) }
+                    todo = item,
+                    deleteTodo = { todoList.removeAt(index) },   //removeAtの引数に配列の要素番目を渡すとその要素を削除出来ます
+                    completeTodo = { todoList[index] = item.copy(isCompoleted = it) }   //item.copy()はdata classのコピーを作成するという意味です
                 )
             }
         }
@@ -143,12 +143,36 @@ fun MyTodoAppContent(
 }
 @Composable
 //() -> Unit は関数型を表しています.()の中にはその関数に必要な引数の型を,->の後には返り値の型を定義します.この場合引数無し.返り値無しの関数を示しています
-fun TodoItem(text: String, deleteTodo: () -> Unit) {
+fun TodoItem(todo: Todo,
+             deleteTodo: () -> Unit,
+             completeTodo: (Boolean) -> Unit) {     //completeTodoのBooleanはチェックボックスがタップされた後の状態を返します
+    val fontColor =
+        if (todo.isCompoleted) {
+            Color.Gray
+        } else {
+            Color.Black
+        }
+    val textDecoration =
+        if (todo.isCompoleted) {
+            TextDecoration.LineThrough  // TextDecoration.LineThroughは文字の中央に線を引く装飾です
+        } else {
+            null
+        }
+
     Row(verticalAlignment = Alignment.CenterVertically) {
-        Text(text = text,
+        Text(
+            text = todo.text,
             modifier = Modifier
                 .padding(vertical = 4.dp)
-                .weight(1f)                 //weight()を使って横幅一杯にTextのComposableを伸ばす事で削除ボタンを右端に固定しています
+                .weight(1f),                 //weight()を使って横幅一杯にTextのComposableを伸ばす事で削除ボタンを右端に固定しています
+            color = fontColor,
+            textDecoration = textDecoration
+        )
+        // Checkboxはチェックボックスを表示するComposableです.checkedでチェックされているか判定します.チェックボックスがタッチされるとonCheckedChangeが走ります
+        Checkbox(
+            checked = todo.isCompoleted,
+            onCheckedChange = completeTodo,
+            modifier = Modifier.padding(start = 4.dp)
         )
         //IconButton()はアイコンをボタンとして表示するためのComposableです
         IconButton(onClick = { deleteTodo() }) {
@@ -166,7 +190,13 @@ fun GreetingPreview() {
         /* ③AndoridStudio側からプレビュー出来るようにUI用の関数を渡す */
         MyTodoAppContent(
             todo = remember { mutableStateOf("文字を入力中...") },
-            todoList = remember { mutableStateListOf("長い長い長い長い長い長い長い長い長い長い長い長い長い TODO", "TODO 2") }
+            todoList = remember {
+                mutableStateListOf(
+                    Todo(text="長い長い長い長い長い長い長い長い長い長い長い長い長い TODO", isCompoleted = false),
+                    Todo(text="完了した TODO", isCompoleted = true)
+                )
+
+            }
         )
     }
 }
